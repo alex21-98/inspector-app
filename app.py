bb4ee03c675f43298bdaa3d97d92e4237e30a6cc
alex21-98 <73622913+alex21-98@users.noreply.github.com>
2026-06-21 21:25:02 -0500

    app.py

--- app.py
+++ app.py
@@ -253,8 +253,8 @@ if uploaded_file is not None:
                                 only_move={'points': 'xy', 'text': 'xy'}, 
                                 expand_points=(1.05, 1.1), 
                                 expand_text=(1.05, 1.1),
-                                force_text=(0.8, 1.2),    # Fuerte empuje horizontal, muy leve vertical
-                                force_points=(0.4, 0.8),  # Igual para la repulsión contra los puntos
+                                force_text=(0.8, 1.3),    # Fuerte empuje horizontal, muy leve vertical
+                                force_points=(0.4, 1.2),  # Igual para la repulsión contra los puntos
                                 arrowprops=dict(arrowstyle='-', color='#78909C', lw=1.2, alpha=0.8, zorder=2), 
                                 max_move=25               # Limita qué tan lejos pueden volar las etiquetas
                             )
